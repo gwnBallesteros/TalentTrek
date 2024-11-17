126fd1295f9ebf90a1028e974104b91bbacf3a3e
gwnBallesteros <gwenballesteros.dev@gmail.com>
2024-11-17 10:43:34 +0800

    Talent trek

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact Page</title>
-</head>
-<body>
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
     <h1>Hello from the Contact Page</h1>
-</body>
-</html>
+</x-layout>
