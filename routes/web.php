ca1d5493e100704768150a2ce91a6a571b5aede6
gwnBallesteros <gwenballesteros.dev@gmail.com>
2024-11-17 15:35:42 +0800

    Talent trek

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,7 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
-class Job {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$40,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$50,000'
-            ]
-        ];
-    }
-}
-
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
@@ -38,9 +16,7 @@ Route::get('/jobs', function (){
 });
 
 Route::get('/jobs/{id}', function ($id){
-
-    $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
-
+    $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
 
